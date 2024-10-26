d980c648bb59073d57c4c2cde4dbca40757c578c
Piotr Steplewski <piotr.steplewski@harman.com>
2024-10-26 14:12:31 +0200

    pojo for pet

--- src/test/java/http/methods/BasicHttpMethods.java
+++ src/test/java/http/methods/BasicHttpMethods.java
@@ -15,25 +15,6 @@ import static io.restassured.RestAssured.given;
 public class BasicHttpMethods {
     Properties properties = new Properties();
 
-    String requestBody1 = "{\n" +
-            "  \"id\": 123,\n" +
-            "  \"category\": {\n" +
-            "    \"id\": 1,\n" +
-            "    \"name\": \"dogs\"\n" +
-            "  },\n" +
-            "  \"name\": \"Burek\",\n" +
-            "  \"photoUrls\": [\n" +
-            "    \"http://photos.com/dog1.jpg\"\n" +
-            "  ],\n" +
-            "  \"tags\": [\n" +
-            "    {\n" +
-            "      \"id\": 1,\n" +
-            "      \"name\": \"dogs-category\"\n" +
-            "    }\n" +
-            "  ],\n" +
-            "  \"status\": \"available\"\n" +
-            "}";
-
     String requestBody2 = "{\n" +
             "  \"id\": 123,\n" +
             "  \"category\": {\n" +
