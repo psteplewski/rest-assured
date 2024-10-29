ef0fce32a4cfff3cfcad94979a822c860d8396f0
Piotr Steplewski <piotr.steplewski@harman.com>
2024-10-29 20:47:15 +0100

    DRY pattern

--- src/test/java/http/methods/PetTests.java
+++ src/test/java/http/methods/PetTests.java
@@ -4,15 +4,16 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.TmsLink;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
-import pojo.pet.Category;
+import org.assertj.core.api.Assertions;
 import pojo.pet.Pet;
-import pojo.pet.Tag;
+import request.configuration.RequestConfigurationBuilder;
 import utils.PetTestDataGenerator;
 import utils.Properties;
 import utils.TestDataGenerator;
 
-import java.util.Collections;
 import io.qameta.allure.restassured.AllureRestAssured;
 
 
@@ -21,52 +22,46 @@ import static io.restassured.RestAssured.given;
 public class PetTests extends TestDataGenerator {
 
     Properties properties = new Properties();
+    Pet actualPet;
+    private final RequestSpecification headers = RequestConfigurationBuilder.getDefaultRequestSpecification();
+    ;
+    private final Pet requestBody = new PetTestDataGenerator().generatePet();
+
 
     @Test
     @Description("Description field")
     @TmsLink("TC-123") //ID test case form Test Management system, configurable in allure.properties
     @Severity(SeverityLevel.CRITICAL)
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
-        Category category = new Category();
-        category.setId(faker().number().numberBetween(1, 9999));
-        category.setName(faker().animal().name());
 
-        Tag tag = new Tag();
-        tag.setId(1);
-        tag.setName(faker().animal().name());
-
-        Pet pet = new Pet();
-        pet.setId(1);
-        pet.setName(faker().animal().name());
-        pet.setCategory(category);
-        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
-        pet.setTags(Collections.singletonList(tag));
-        pet.setStatus("available");
-
-        given().filter(new AllureRestAssured()).log().all().body(pet).contentType("application/json")
+        actualPet = given().spec(headers)
+                .body(requestBody).contentType("application/json")
                 .when().post(properties.baseUrl + "/v2/pet")
-                .then().log().all().statusCode(200);
+                .then().statusCode(HttpStatus.SC_OK).log().all()
+                .extract().as(Pet.class);
+
+        Assertions.assertThat(actualPet).describedAs("Send Pet was different than received by API").isEqualToComparingFieldByFieldRecursively(requestBody);
     }
 
     @Test
     public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
-        given().filter(new AllureRestAssured()).log().all().pathParam("petId", 1).
-                when().baseUri(properties.baseUrl + "/v2/pet/{petId}").
-                then().statusCode(200).log().all();
+        given().spec(headers).pathParam("petId", 1).
+                when().get(properties.baseUrl + "/v2/pet/{petId}").
+                then().statusCode(HttpStatus.SC_OK).log().all();
     }
 
     @Test
     public void putRequest() {
-        Pet petRequestBody = new PetTestDataGenerator().generatePet();
-        given().filter(new AllureRestAssured()).log().all().body(petRequestBody).contentType("application/json")
+        given().spec(headers)
+                .body(requestBody)
                 .put(properties.baseUrl + "/v2/pet")
-                .then().log().all().statusCode(200);
+                .then().statusCode(HttpStatus.SC_OK).log().all();
     }
 
     @Test
     public void deleteRequest() {
-        given().filter(new AllureRestAssured()).log().all().pathParam("petId", 1)
+        given().spec(headers).pathParam("petId", 1)
                 .delete(properties.baseUrl + "/v2/pet/{petId}")
-                .then().log().all().statusCode(200);
+                .then().statusCode(HttpStatus.SC_OK).log().all();
     }
 }
